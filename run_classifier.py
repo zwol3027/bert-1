ff98e0233fb3ca8171f00b54f9602840c82bcfab
zwol3027 <zwol3027@uni.sydney.edu.au>
2019-06-18 16:06:02 +0530

    add comma

--- run_classifier.py
+++ run_classifier.py
@@ -690,15 +690,16 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
         precision = tf.metrics.precision(labels=label_ids, predictions=predictions, weights=is_real_example)
         recall = tf.metrics.recall(labels=label_ids, predictions=predictions, weights=is_real_example)
         f = tf.contrib.metrics.f1_score(labels=label_ids, predictions=predictions, weights=is_real_example)
-        f_sklearn = sklearn.metrics.accuracy_score(y_true=label_ids, y_pred=predictions)
+        # f_sklearn = sklearn.metrics.accuracy_score(y_true=label_ids, y_pred=predictions)
+        print(len(predictions),len(label_ids))
 
         return {
             "eval_accuracy": accuracy,
             "eval_loss": loss,
             "eval_precision": precision,
             "eval_recall": recall,
-            "eval_f": f,
-            "eval_f_sklearn": f_sklearn
+            "eval_f": f
+            # "eval_f_sklearn": f_sklearn
 
         }
 
